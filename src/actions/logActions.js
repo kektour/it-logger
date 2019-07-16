9f38c51d291dd0e1145a8cfce75780fd41e509b9
Alexey <elexex97@gmail.com>
2019-07-16 22:39:09 +0300

    FEATURE: Logs Reducer, Actions & Types

---
+++ src/actions/logActions.js
@@ -0,0 +1,28 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+
+export function getLogs() {
+  return async function(dispatch) {
+    try {
+      setLoading();
+
+      const res = await fetch('/logs');
+      const data = await res.json();
+
+      dispatch({
+        type: GET_LOGS,
+        payload: data
+      });
+    } catch (err) {
+      dispatch({
+        type: LOGS_ERROR,
+        payload: err.response.data
+      });
+    }
+  };
+}
+
+export function setLoading() {
+  return {
+    type: SET_LOADING
+  };
+}
